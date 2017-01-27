9f64faa5b8c01322e40b0812dc8f6431bdd4ff67
Arman Yeghiazaryan <otanim617@gmail.com>
2017-01-27 19:50:26 +0400

    odd staff fix ?

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const languageList = ['en', 'hy', 'ru', 'it'];
 
-var languages = {};
+const languages = {};
 languageList.forEach((language) => {
   languages[language] = require('./languages/' + language);
 });
