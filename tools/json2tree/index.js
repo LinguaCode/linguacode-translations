ca0999bcc66c3f654b58f6900461faa66da7225b
Arman Yeghiazaryan <otanim617@gmail.com>
2017-03-11 19:39:15 +0400

    es5 fix

--- tools/json2tree/index.js
+++ tools/json2tree/index.js
@@ -9,20 +9,17 @@ var OUTPUT_FOLDER_NAME = 'output';
 var OUTPUT_FOLDER_PATH = path.join(__dirname, OUTPUT_FOLDER_NAME);
 
 fs.readdir(path.join(__dirname, INPUT_FOLDER_NAME), function (err, list) {
-  list.forEach((languageCode) => {
+  list.forEach(function (languageCode) {
     if (fs.lstatSync(path.join(__dirname, INPUT_FOLDER_NAME, languageCode)).isDirectory()) {
-      var coreJSON = require(`./${INPUT_FOLDER_NAME}/${languageCode}/core.json`);
+      var coreJSON = require('./' + INPUT_FOLDER_NAME + '/' + languageCode + '/core.json');
 
       var modifiedFileText = originalFile;
       for (var key in coreJSON) {
         var constantCommandName = key.slice(1).toUpperCase();
-        modifiedFileText = modifiedFileText.replace(
-          new RegExp(`command\\:\\s*COMMAND.${constantCommandName},\\s+definition\\:\\s+'[a-zA-Z]+'`, 'g'),
-          `command: COMMAND.${constantCommandName},\n  definition: '${coreJSON[key]}'`
-        );
+        modifiedFileText = modifiedFileText.replace(new RegExp('command\\:\\s*COMMAND.' + constantCommandName + ',\\s+definition\\:\\s+\'[a-zA-Z]+\'', 'g'), 'command: COMMAND.' + constantCommandName + ',\n  definition: \'' + coreJSON[key] + '\'');
       }
 
-      fs.writeFileSync(path.join(OUTPUT_FOLDER_PATH, `${languageCode}.js`), modifiedFileText);
+      fs.writeFileSync(path.join(OUTPUT_FOLDER_PATH, languageCode + '.js'), modifiedFileText);
     }
   });
 });
--- tools/languageListGenerate/index.js
+++ tools/languageListGenerate/index.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 var LANGUAGE_FOLDER_PATH = '../../languages';
 
 fs.readdir(path.join(__dirname, LANGUAGE_FOLDER_PATH), function (err, list) {
-  list = list.map(language=>language.substring(0, language.indexOf('.')));
+  list = list.map(function (language) {
+    return language.substring(0, language.indexOf('.'));
+  });
 
-  fs.writeFileSync(`languageList.js`, `module.exports = ['${list.join('\', \'')}'];`);
+  fs.writeFileSync('languageList.js', 'module.exports = [\'' + list.join('\', \'') + '\'];');
 });
